b828ffc8fd89a46be0d857adcfa25a5bf5016259
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-12-12 22:38:12 -0300

    Function Vibration implemented

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,38 +1,49 @@
 import React, { useState } from "react"
-import { View, Text, TextInput, TouchableOpacity } from "react-native"
+import { View, Text, TextInput, TouchableOpacity, Vibration } from "react-native"
 import ResultImc from "./ResultImc";
 import styles from "./style";
 
 export default function Form(){
 
-const [height, setHeight] = useState(null)
-const [weight, setWeight] = useState(null)
-const [messageImc, setMessageImc] = useState("Preencha o peso e altura")
-const [imc, setImc] = useState(null)
-const [textButton, setTextButton] = useState("Calcular")
+    const [height, setHeight] = useState(null);
+    const [weight, setWeight] = useState(null);
+    const [messageImc, setMessageImc] = useState("Preencha o peso e altura");
+    const [imc, setImc] = useState(null);
+    const [textButton, setTextButton] = useState("Calcular");
+    const [errorMessage, setErrorMessage] = useState(null);
 
-function imcCalculator() {
-    return setImc((weight/(height*height)).toFixed(2))
-}
-
-function validationImc() {
-    if(weight != null && height != null) {
-        imcCalculator()
-        setHeight(null)
-        setWeight(null)
-        setMessageImc("Seu IMC é igual: ")
-        setTextButton("Calcular Novamente")
-        return
+    function imcCalculator() {
+        return setImc((weight/(height*height)).toFixed(2))
     }
-    setImc(null)
-    setTextButton("Calcular")
-    setMessageImc("Preencha o peso e altura")
-}
+
+    function verificationImc() {
+        if(imc == null){
+            Vibration.vibrate();
+            setErrorMessage("campo obrigatório*")
+        }
+    }
+
+    function validationImc() {
+        if(weight != null && height != null) {
+            imcCalculator()
+            setHeight(null)
+            setWeight(null)
+            setMessageImc("Seu IMC é igual: ")
+            setTextButton("Calcular Novamente")
+            setErrorMessage(null)
+            return
+        }
+        verificationImc()
+        setImc(null)
+        setTextButton("Calcular")
+        setMessageImc("Preencha o peso e altura")
+    }
 
     return(
         <View style={styles.formContext}>
             <View style={styles.form}>
                 <Text style={styles.formLabel}>Altura</Text>
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
                 <TextInput
                     style={styles.input}
                     onChangeText={setHeight}
@@ -41,6 +52,7 @@ function validationImc() {
                     keyboardType="numeric"
                 />
                 <Text style={styles.formLabel}>Peso</Text>
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
                 <TextInput
                     style={styles.input}
                     onChangeText={setWeight}
