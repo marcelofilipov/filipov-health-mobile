b8e386d4c79a362f193958fc311d3b1798cb8180
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-12-12 22:58:23 -0300

    API Share implemented

--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -19,6 +19,22 @@ const styles = StyleSheet.create({
         color:"#ff0043",
         fontWeight:"bold",
     },
+    boxShareButton:{
+        width:"100%",
+        alignItems:"center",
+        marginBottom:10,
+    },
+    shared:{
+        backgroundColor:"#1877f2",
+        borderRadius:50,
+        paddingBottom:5,
+        paddingTop:5,
+    },
+    sharedText:{
+        color:"#fff",
+        fontWeight:"bold",
+        paddingHorizontal:30,
+    }
 });
 
 export default styles
