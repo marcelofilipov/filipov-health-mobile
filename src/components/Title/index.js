23451886e8373e3241ead2737cdbf866b7b3a8bf
Marcelo Filipov <marcelo.filipov@gmail.com>
2021-12-12 21:12:53 -0300

    Style Title implemented

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { View, Text } from "react-native"
+import styles from "./style";
 
 export default function Title(){
     return(
-        <View>
-            <Text>FILIPOV HEALTH</Text>
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>FILIPOV HEALTH</Text>
         </View>
     );
 }
